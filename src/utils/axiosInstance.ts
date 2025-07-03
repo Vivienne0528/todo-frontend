0e8b6af38cdf76c8bddbbfaccd02f0833ea42b91
YuweiGu98 <vivienne0528.gu@gmail.com>
2025-07-03 17:11:36 +1000

    set up backend url

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3500",
+  baseURL: "https://todo-backend-6ed0.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
